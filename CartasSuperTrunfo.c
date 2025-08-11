38a9525b85474550f7059ffded16941016d6107e
caroolrecchi <caroolehlert@gmail.com>
2025-08-11 18:04:13 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@ int main(){
 char estado[10] = "Alagoas";
 char codigo[4] = "A01";
 char cidade[100] = "Maceió";
-int populacao = 1;
+float populacao = 1.000.000;
 float area = 503;
 float pib = 27.480;
 int pontosturisticos = 50;
@@ -17,7 +17,7 @@ int pontosturisticos = 50;
 char estado2[10] = "Paraná";
 char codigo2[4] = "B02";
 char cidade2[100] = "Curitiba";
-int populacao2 = 1.773;
+float populacao2 = 1.773.000;
 float area2 = 2;
 float pib2 = 98;
 int pontosturisticos2 = 25;
@@ -28,65 +28,50 @@ int pontosturisticos2 = 25;
 printf("Leitura da Carta 1:\n");
 
 printf("Digite o Estado: \n");
-scanf("%s", &estado); 
-printf("%s\n", estado);
+scanf("%s", estado); 
 
 printf("Digite o Código: \n");
-scanf("%s", &codigo);
-printf("%s\n", codigo);
+scanf("%s", codigo);
 
 printf("Digite a Cidade: \n");
-scanf("%s", &cidade);
-printf("%s\n", cidade);
+scanf("%s", cidade);
 
-printf(" Informe a População (em milhões): \n");
-scanf("%d", &populacao);
-printf("%d\n", populacao);
+printf(" Informe a População: \n");
+scanf("%f", &populacao);
 
 printf(" Informe a Área (em km²): \n");
-scanf("%f", &area);
-printf("%f\n", area);
+scanf("%.2f", &area);
 
 printf("Informe o PIB (em bilhões): \n");
 scanf("%f", &pib);
-printf("%f\n", pib);
 
 printf(" Digite o Número de Pontos Turísticos: \n");
 scanf("%d", &pontosturisticos);
-printf("%d\n", pontosturisticos);
 
 //Leitura da Carta 2:
 
 printf("Leitura da Carta 2:\n");
 
 printf("Digite o Estado: \n");
-scanf("%s", &estado2); 
-printf("%s\n", estado2);
+scanf("%s", estado2); 
 
 printf("Digite o Código: \n");
-scanf("%s", &codigo2);
-printf("%s\n", codigo2);
+scanf("%s", codigo2);
 
 printf("Digite a Cidade: \n");
-scanf("%s", &cidade2);
-printf("%s\n", cidade2);
+scanf("%s", cidade2);
 
-printf(" Informe a População (em milhões): \n");
-scanf("%d", &populacao2);
-printf("%d\n", populacao2);
+printf(" Informe a População: \n");
+scanf("%f", &populacao2);
 
 printf(" Informe a Área (em km²): \n");
-scanf("%f", &area2);
-printf("%f\n", area2);
+scanf("%.2f", &area2);
 
 printf("Informe o PIB (em bilhões): \n");
 scanf("%f", &pib2);
-printf("%f\n", pib2);
 
 printf(" Digite o Número de Pontos Turísticos: \n");
 scanf("%d", &pontosturisticos2);
-printf("%d\n", pontosturisticos2);
-
 
 
 return 0;
